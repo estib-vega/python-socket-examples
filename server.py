c3423802201ce0fb3f359755a1811d85a0b988ac
Esteban Vega <stron@me.com>
2018-05-17 21:17:29 +0200

    -Added connection handler for multithreading -Added multithreading. The while-loop doesn't work. It performs the command interpretation once and the it closes the connection

--- server.py
+++ server.py
@@ -1,9 +1,41 @@
 # python socket test
 # server
+
+def connection_handler(conn, addr, c):
+    print("initiated connection from:", addr)
+    with conn:
+        while True:
+            # returns the sent data buffered in 1024 bytes
+            data = conn.recv(16)
+
+            if not data: break # end current connection
+
+            print(data.decode())
+
+            response = -1
+
+            # interpret command
+            command = data.decode()
+            if command == "i":
+                print("increment")
+                response = c.increment()
+            elif command == "r":
+                print("reset")
+                response = c.reset()
+            elif command == "d":
+                print("decrement")
+                response = c.decrement()
+            else:
+                print("not yet suppported... sry")
+            
+            conn.sendall(response.to_bytes(16, byteorder=sys.byteorder, signed=True))
+            return
+
 if __name__ == '__main__':
     import socket
     import sys
     from counter import Counter
+    import _thread as t
 
     c = Counter()
 
@@ -18,40 +50,16 @@ if __name__ == '__main__':
         # socket object bound to the socket address
         socket.bind((HOST, PORT)) 
 
+        # number of unaccepted connections
+        # before start to refusing connections
+        # -> one connection at a time
+        socket.listen(2)
+
         # outter loop for running sequentially for every request
         while True:
-            # number of unaccepted connections
-            # before start to refusing connections
-            # -> one connection at a time
-            socket.listen(1)
+            
             print("listening in localhost at port", PORT, "...")
 
             conn, addr = socket.accept()
 
-            with conn: # close connection after the code-block is finished
-                print("initiated connection from:", addr)
-                while True:
-                    # returns the sent data buffered in 1024 bytes
-                    data = conn.recv(16)
-
-                    if not data: break # end current connection
-
-                    print(data.decode())
-
-                    response = -1
-
-                    # interpret command
-                    command = data.decode()
-                    if command == "i":
-                        print("increment")
-                        response = c.increment()
-                    elif command == "r":
-                        print("reset")
-                        response = c.reset()
-                    elif command == "d":
-                        print("decrement")
-                        response = c.decrement()
-                    else:
-                        print("not yet suppported... sry")
-                    
-                    conn.sendall(response.to_bytes(16, byteorder=sys.byteorder, signed=True))
+            t.start_new_thread(connection_handler, (conn, addr, c))
